5ef2497c4a8972454756cd3a74ce402503ca746e
Andrew Littlefield <andrew.ray.allan@gmail.com>
2017-05-31 14:55:14 -0600

    completed 1st and second obj

---
+++ hover.js
@@ -0,0 +1,10 @@
+
+// let hover = document.getElementById('wow');
+// hover.addEventListener("mouseover", funk);
+// function hover() {
+
+// }
+
+document.getElementById('wow').addEventListener('mouseover', function (e) {
+	alert("HEY! I told you not to hover over me!");
+});
---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+let button = document.getElementById('bt1');
+button.addEventListener("click", one);
+function one () {
+	document.getElementById('speak').innerHTML = "I'm right!";
+};
+
+let butt = document.getElementById('bt2');
+butt.addEventListener("click", two);
+function two () {
+	document.getElementById('speak').innerHTML = "No, I'm right!!";
+};
+
+// document.getElementsByTagName('button').addEventListener ('click', no);
+// function no () {
+// 	document.getElementById('speak').innerHTML += "I'm right!"
+// //	document.getElementById('speak').innerHTML += "No I'm right!"
+
+// };
