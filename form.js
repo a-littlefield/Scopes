c23484e9d43f374a26a024a4d740d2b32c4afdd9
Andrew Littlefield <andrew.ray.allan@gmail.com>
2017-05-31 16:38:00 -0600

    finished final assigment after some struggle

---
+++ form.js
@@ -0,0 +1,11 @@
+let submit = document.getElementById('submit');
+let password = document.getElementById('pass');
+let username = document.getElementById('user').value;
+submit.addEventListener('click', trunk);
+function trunk() {
+	if (password.value == '12345678' && username.length < 14) {
+		document.getElementById('in').innerHTML = 'Great job on knowing your username and password!'
+	}else {
+		alert('Incorrect username or password')
+	}
+}
